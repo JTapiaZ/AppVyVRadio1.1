5861c51b6c4e72109fca6c67fbb5c9184d4b03bb
JTapiaZ <juanjotapias0414@gmail.com>
2024-08-15 00:04:57 -0500

    VyVRadioApp Publicada 15082024

---
+++ model/data.js
@@ -0,0 +1,25 @@
+const songs = [
+  {
+    id: 1,
+    title: 'Verdad y Vida Radio 870AM',
+    artist: 'Medellín - Antioquia',
+    artwork: require('../assets/img/870.jpg'),
+    url: 'https://stream.zeno.fm/zznllllp06cuv',
+  },
+  {
+    id: 2,
+    title: 'Verdad y Vida Radio 100.1FM',
+    artist: 'Aguadas Caldas - Antioquia',
+    artwork: require('../assets/img/1001.jpg'),
+    url: 'https://stream.zeno.fm/vvon5xkmvhwvv',
+  },
+  {
+    id: 3,
+    title: 'Verdad y Vida Radio Online',
+    artist: 'Disponible las 24hrs',
+    artwork: require('../assets/img/online.jpg'),
+    url: 'https://stream.zeno.fm/n590rdbh62uuv',
+  },
+];
+
+export default songs;
